494108cb64a25370aab6d8c8949b9ca4b3f3a162
Hilal <156501992+hilalbht@users.noreply.github.com>
2025-12-22 07:06:02 +0000

    Streamlit UI for XGBoost cirrhosis stage prediction

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ le_stage = joblib.load("stage_label_encoder.pkl")
 # =========================
 st.markdown("""
 <div class="header-card">
-    <h1>"Klinik Parametrelere Dayalı<br>Siroz Evre Tahmin Sistemi"</h1>
+    <h1>Klinik Parametrelere Dayalı<br>Siroz Evre Tahmin Sistemi</h1>
     <p><b>⚠️Eğitim ve klinik simülasyon amaçlıdır.</b></p>
     <p style="font-size:14px;">
         ⚠️Bu sistem <b>olasılıksal ve istatistiksel bir tahmin</b> üretir.  
